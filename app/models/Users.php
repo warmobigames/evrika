6baeece5437be07c7ff2c121de08d3b3f34c306c
warmobigames <warmobigames@gmail.ru>
2015-06-12 04:40:51 +0400

    first commit

---
+++ app/models/Users.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Warmobi\Models;
+
+class Users extends \Phalcon\Mvc\Model
+{
+    public $id;
+    
+    public $username;
+    
+    public $key;
+    
+    public static function find($p = array())
+    {
+        return parent::find($p);
+    }
+
+    public static function findFirst($p = array())
+    {
+        return parent::findFirst($p);
+    }
+
+    public static function login($key)
+    {
+        $user = Users::findFirst('key = "'.$key.'"');
+        return $user;
+    }
+    
+}
